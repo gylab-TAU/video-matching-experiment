590317e7cccf66780e391faf731f57c054b778b8
Gali <galigeller@mail.tau.ac.il>
2021-12-29 13:38:18 +0200

    fixed familiarity

--- src/components/familiarityComponent.js
+++ src/components/familiarityComponent.js
@@ -1,11 +1,11 @@
 import  HtmlButtonResponsePlugin from '@jspsych/plugin-html-button-response';
 
 class familiarityComponent {
-    static getTrial(image) {
+    static getTrial() {
         let imageHeight = Math.round(window.screen.availHeight / 2);
         let trial = {
             type: HtmlButtonResponsePlugin,
-            stimulus: this.getImage(image),
+            stimulus: jsPsych.timelineVariable("stim"),
             choices: ['yes', 'no'],
             prompt: '<p> Do you know who this person is? </p>',
             response_ends_trial: true,
@@ -14,14 +14,6 @@ class familiarityComponent {
         };
 
         return trial;
-    }
-
-    static getImage(source) {
-        return(
-            '<div class="image">' +
-            '<img src=' + source + " " +  '>' +
-            '</div>'
-        );
     }
 }
 
--- src/procedures/familiarityProcedure.js
+++ src/procedures/familiarityProcedure.js
@@ -2,19 +2,40 @@ import * as familiarityComponent from "../components/familiarityComponent";
 
 export class familiarityProcedure {
     constructor(images) {
-        this.timeline = [];
-
-        for (let i = 0; i < images.length; i++) {
-            this.timeline.push(familiarityComponent.default.getTrial(images[i]))
-        }
+        this.images = images;
     }
 
     getProcedure() {
         let procedure = {
-            timeline: this.timeline,
+            timeline: [familiarityComponent.default.getTrial()],
+            randomize_order: true,
+            timeline_variables: this.getTimelineVariables(),
             randomize_order: true
         }
 
+        console.log(procedure)
+
         return procedure;
+    }
+
+    getTimelineVariables() {
+        let timelineVariables = [];
+
+        for (let i = 0; i < this.images.length; i++) {
+            let path = this.images[i];
+
+            let element =
+                '<div class="image">' +
+                '<img src=' + path + " " +  '>' +
+                '</div>';
+
+            let elementObject = {stim: element};
+
+            timelineVariables.push(elementObject);
+        }
+
+        console.log(timelineVariables)
+
+        return timelineVariables;
     }
 }
